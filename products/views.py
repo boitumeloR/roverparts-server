bb6bd231517d4662e468b75371b72fb9836950b5
Boitumelo Rampete <u17120323@tuks.co.za>
2021-08-14 23:13:02 +0200

    new urls

--- products/views.py
+++ products/views.py
@@ -4,10 +4,10 @@ from rest_framework.response import Response
 import json
 import requests
 # Create your views here.
-main_url = "https://b4f652c3b43475ee5b72347a36f62f19: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@roverpartsza.myshopify.com/admin/api/2021-07/products.json"
+main_url = "https://04f0872440bcf48fb51c5f3dff957582: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@roverpartssa.myshopify.com/admin/api/2021-07/products.json"
 class ProductDetail(APIView):
     def get(self, request, id, format = None):
-        url = f"https://b4f652c3b43475ee5b72347a36f62f19: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@roverpartsza.myshopify.com/admin/api/2021-07/products/{id}.json"
+        url = f"https://04f0872440bcf48fb51c5f3dff957582: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@roverpartssa.myshopify.com/admin/api/2021-07/products/{id}.json"
         r = requests.get(url)
         print(r.json())
         return Response(r.json())
@@ -18,12 +18,12 @@ class ProductDetail(APIView):
         return Response(r.json())
     
     def put(self, request, id, format = None):
-        url = f"https://b4f652c3b43475ee5b72347a36f62f19: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@roverpartsza.myshopify.com/admin/api/2021-07/products/{id}.json"
+        url = f"https://04f0872440bcf48fb51c5f3dff957582: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@roverpartssa.myshopify.com/admin/api/2021-07/products/{id}.json"
         r = requests.put(url, json = json.loads(str(request.body, encoding = 'utf-8')))
         return Response(r.json())
 
     def delete(self, request, id, format = None):
-        url = f"https://b4f652c3b43475ee5b72347a36f62f19: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@roverpartsza.myshopify.com/admin/api/2021-07/products/{id}.json"
+        url = f"https://04f0872440bcf48fb51c5f3dff957582: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@roverpartssa.myshopify.com/admin/api/2021-07/products/{id}.json"
         r = requests.delete(url)
         return Response(r.json())
 
